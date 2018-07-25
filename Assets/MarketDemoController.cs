da419a3fcc248122eb75f1b6de53d1c0b7e391d2
Sasha2018 <37545340+Sasha2018@users.noreply.github.com>
2018-07-25 10:56:38 +0300

    ADD plugin for mobile with demo scene

--- Assets/MarketDemoController.cs
+++ Assets/MarketDemoController.cs
@@ -1,12 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class MarketDemoController : MonoBehaviour
 {
+    [SerializeField] private float _sizeDelta;
+    [SerializeField] private Button _btnSizeMinusX;
+    [SerializeField] private Button _btnSizeMinusY;
+    [SerializeField] private Button _btnSizePlusX;
+    [SerializeField] private Button _btnSizePlusY;
+    [SerializeField] private UniWebView _webUniWebView;
 
-    public Button LoadTransparentWindow;
-    public Button NativeWindow;
-    public Button Close;
+    private void Awake()
+    {
+        _btnSizePlusX.onClick.AddListener(() =>
+            ChangeContentSize(new Vector2(_sizeDelta, 0)));
+        _btnSizeMinusX.onClick.AddListener(() =>
+            ChangeContentSize(new Vector2(-_sizeDelta, 0)));
+        _btnSizePlusY.onClick.AddListener(() =>
+            ChangeContentSize(new Vector2(0, _sizeDelta)));
+        _btnSizeMinusY.onClick.AddListener(() =>
+            ChangeContentSize(new Vector2(0, -_sizeDelta)));
+    }
+
+    private void ChangeContentSize(Vector2 value)
+    {
+        var content = _webUniWebView.ReferenceRectTransform;
+        var newSize = content.sizeDelta + value;
+        content.sizeDelta = newSize;
+        _webUniWebView.UpdateFrame();
+    }
 }
